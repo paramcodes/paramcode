330f78409ec9b9d94d73d652cc03a435cf59ceb3
paramcodes <sparamveer1001@gmail.com>
2026-04-16 12:37:39 +0530

    added auth

---
+++ apps/web/app/components/authbutton.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <p>{session.user?.email}</p>
+        <button onClick={() => signOut()}>Logout</button>
+      </>
+    );
+  }
+
+  return (
+    <button onClick={() => signIn("google")}>
+      Sign in with Google
+    </button>
+  );
+}
