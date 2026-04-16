477d7a71fd0f6e994448e2ace55314c8e43d8374
paramcodes <sparamveer1001@gmail.com>
2026-04-16 12:38:27 +0530

    auth added

---
+++ apps/api/route.ts
@@ -0,0 +1,10 @@
+import prisma from "./lib/prisma";
+
+await prisma.user.create({
+  data: {
+    name: "Param",
+    email: "param@example.com",
+  },
+});
+
+const users = await prisma.user.findMany();
