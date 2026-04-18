f470015c60f5cb8d5478a891f22bd14e92836816
paramcodes <sparamveer1001@gmail.com>
2026-04-18 10:14:21 +0530

    added ui

--- apps/web/app/api/progress/route.ts
+++ apps/web/app/api/progress/route.ts
@@ -1,6 +1,6 @@
 import prisma from "../lib/prisma";
 import { getServerSession } from "next-auth";
-import {authOptions} from "../lib/auth.ts"
+import {authOptions} from "../lib/auth"
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
